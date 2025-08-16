b438e21e2ad61ab8a8532f275cad7b498aa795da
Omid Ahmadzadeh <omid4mit@gmail.com>
2025-08-16 19:51:58 +0330

    First Version, Using lopp to train all models.

---
+++ Machine_Learning/All_Machine_Learning_Methods/supervisedLearningClassification.py
@@ -0,0 +1,32 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_classification
+import time
+start_time = time.time()
+
+X, y = make_classification(n_samples=100, n_features=5, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+models = {
+    "Logistic Regression": LogisticRegression(),
+    "KNN": KNeighborsClassifier(),
+    "SVM": SVC(),
+    "Decision Tree": DecisionTreeClassifier(),
+    "Random Forest": RandomForestClassifier(),
+    "Naïve Bayes": GaussianNB(),
+    "MLP Classifier": MLPClassifier()
+}
+
+for name, model in models.items():
+    model.fit(X_train, y_train)
+    print(f"{name}: {model.score(X_test, y_test):.2f}")
+
+print("--- %s seconds ---" % (time.time() - start_time))
+print('New')
+print('New2')
